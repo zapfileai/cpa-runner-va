f17106282b12357b4a5de533bb7f041bda8b1c7d
Sreehari Annam <sannam@wescodist.com>
2026-06-30 06:56:49 -0400

    feat: full run — 80 cities, 16 batches, 5 queries, auto PR merge

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
 const virginiaCities = [
-  "Richmond",
+  "Ashburn", "Richmond", "Chesapeake", "Fairfax Station", "Fredericksburg",
+  "Chantilly", "Concord", "Bumpass", "Franklin", "Check",
+  "Chesterfield", "Bristol", "Accomac", "Afton", "Blackwater",
+  "Bluefield", "Charlottesville", "Catawba", "Bluemont", "Catlett",
+  "Christiansburg", "Callands", "Cobbs Creek", "Clear Brook", "Atlantic",
+  "Buena Vista", "Church Road", "Clarksville", "Branchville", "Alton",
+  "Ashland", "Cape Charles", "Clifton", "Alexandria", "Henrico",
+  "Virginia Beach", "Falls Church", "Springfield", "Glen Allen", "Danville",
+  "Leesburg", "Gloucester", "Manassas", "Sterling", "Hampton",
+  "Warrenton", "Blackstone", "Catharpin", "Boyce", "Big Stone Gap",
+  "Centreville", "Boones Mill", "Covesville", "Aylett", "Bedford",
+  "Cloverdale", "Ben Hur", "Norfolk", "Galax", "Reston",
+  "Arlington", "Bowling Green", "Mclean", "Burgess", "Coeburn",
+  "Burke", "Broadway", "Cardinal", "Copper Hill", "Buckingham",
+  "Vienna", "Herndon", "Lorton", "Pennington Gap", "Winchester",
+  "Roanoke", "Castlewood", "Berryville", "Axton", "Colonial Beach",
 ];
 
 const BATCH_SIZE = 5;
